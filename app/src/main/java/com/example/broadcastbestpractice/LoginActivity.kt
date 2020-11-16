f2dd806586d9386dfcd9d7383de97724ab627831
xushunyu <2631582891@qq.com>
2020-11-16 10:11:28 +0800

    完成了登录界面设计

---
+++ app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
@@ -0,0 +1,10 @@
+package com.example.broadcastbestpractice
+
+import android.os.Bundle
+
+class LoginActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
