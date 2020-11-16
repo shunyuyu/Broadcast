737658e2d8c1dcde58869a5f45bbcb60779d348a
xushunyu <2631582891@qq.com>
2020-11-16 10:58:09 +0800

    修改了APP图标，实现了登录功能

--- app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
+++ app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
@@ -1,10 +1,24 @@
 package com.example.broadcastbestpractice
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        LoginButton.setOnClickListener {
+            val id = UserID.text.trim().toString()
+            val pwd = UserPwd.text.trim().toString()
+            if (id == "2631582891" && pwd == "123456"){
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this,"QQ或者密码错误，请重试",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
